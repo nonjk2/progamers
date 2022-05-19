3075731f1668b273441be99382a2a932ccd64cbc
최은석 <trgf456@naver.com>
2022-05-19 20:22:06 +0900

    5/19 +

--- Lv1/제일 작은수 제거하기.js
+++ Lv1/제일 작은수 제거하기.js
@@ -1,16 +1,30 @@
-let n = [1,2,3,5,3,4,7,2,1];
-// const a = n.sort()[0]
-
-
-
-// let answer = n.splice(a.sort()[0], 1)
-const answer = n.map((e,i,arr)=> e===1 ? 0  : e )
-// while (n.includes(a.sort()[0])) {
-//     n.slice(a.sort()[0])
-//     break;
-// }
-console.log(answer)
+const n = [2,3,3,2,3,2,2,2,3,4];
+// const a = n.sort()
+// console.log(n.splice)
 // const solution = n => {
-    
-//     return
+//     const a = n.concat()
+//     while (n.includes(a.sort()[0])) {
+//         n.length===1 ? n=[-1] : n.splice(n.indexOf(a.sort()[0]),1) 
+//     }
+//     return n    
 // }
+
+// const solution = n => {
+//     const a = n.concat()
+//     n.length===1 ? n=[-1] : n.splice(n.indexOf(a.sort()[0]),1)
+//     return n
+// }
+
+const solution = n => {
+
+    let source = n[0];
+    for (let i = 0; i < n.length; i++) {
+        n[i] <= source ? source = n[i] : false
+    }
+    n.splice(n.indexOf(source),1)
+    if (n.length === 0) {n = [-1]}
+    return n  
+}
+
+console.log(JSON.stringify(solution(n)) ) 
+
